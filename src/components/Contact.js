70df7c94e91008e123944f225db36987eb58960f
cricgok <rgokul.aids2021@citchennai.net>
2024-07-22 14:06:20 +0530

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -30,7 +30,7 @@ export const Contact = () => {
     setButtonText("Sending...");
     
     try {
-      let response = await fetch("http://localhost:5000/contact", {
+      let response = await fetch("https://portfolio-backend-1-zal6.onrender.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
